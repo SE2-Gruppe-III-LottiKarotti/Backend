5d337225de2734f6a07807462883f281ecae3c1f
dmalle1990 <115376757+dmalle1990@users.noreply.github.com>
2024-04-16 23:50:01 +0200

    spielerMessage init

---
+++ src/main/java/at/aau/serg/websocketdemoserver/msg/SpielerMessage.java
@@ -0,0 +1,40 @@
+package at.aau.serg.websocketdemoserver.msg;
+
+public class SpielerMessage {
+
+    private ActionType actionType;
+    private String spielerID;
+    private String name;
+
+    //constructor or default constructor???
+    public SpielerMessage () {
+    }
+
+    public enum ActionType {
+        LOGIN, LOGIN_OK, LOGIN_ERR
+    }
+
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(ActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getSpielerID() {
+        return spielerID;
+    }
+
+    public void setSpielerID(String spielerID) {
+        this.spielerID = spielerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
