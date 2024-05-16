b89935e1216b97cda07ff680fa332eb549538b21
danieltabatabai <danieltabse@gmail.com>
2024-05-16 13:58:23 +0200

    Chat Censor Tests

---
+++ src/main/java/at/aau/serg/websocketdemoserver/msg/CensorMessage.java
@@ -0,0 +1,23 @@
+package at.aau.serg.websocketdemoserver.msg;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CensorMessage {
+
+    public static String censorText(String text, String[] censoredWords) {
+        for (String word : censoredWords) {
+            //regex pattern
+            //matcht groß und kleinschreibung --> einstellbar bei CASE_INSENSITIVE --> auch andere Parameter möglich
+            Pattern pattern = Pattern.compile("\\b" + Pattern.quote(word) + "\\b", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(text);
+            String sign = "*";
+            String replacement = sign.repeat(word.length());
+            text = matcher.replaceAll(replacement);
+        }
+        return text;
+    }
+
+}
+
+
