05cdef14bbd8342ca2d35ec1150d0d12264c80eb
Baton Berishaj <batonbe@edu.aau.at>
2024-05-19 17:28:12 +0200

    Room correction

--- src/main/java/at/aau/serg/websocketdemoserver/model/raum/Room.java
+++ src/main/java/at/aau/serg/websocketdemoserver/model/raum/Room.java
@@ -27,7 +27,6 @@ public class Room {
         this.availablePlayersSpace = maxPlayers;
         this.listOfPlayers = new ArrayList<>();
         this.cheaters = new ArrayList<>();
-        //TODO: vll auch das gameboard hier initialisieren...
         this.gameboard = new Gameboard();
     }
 
