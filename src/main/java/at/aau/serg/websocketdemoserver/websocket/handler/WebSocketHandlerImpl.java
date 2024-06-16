8049142d84ac5de6412117d641764054850b8757
dmalle1990 <115376757+dmalle1990@users.noreply.github.com>
2024-06-16 02:06:53 +0200

    seperated handlerJoinRoom & tested it

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/handler/WebSocketHandlerImpl.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/handler/WebSocketHandlerImpl.java
@@ -10,6 +10,7 @@ import at.aau.serg.websocketdemoserver.repository.InMemoryRoomRepo;
 
 import at.aau.serg.websocketdemoserver.websocket.handler.defaults.HandlerHeartbeat;
 import at.aau.serg.websocketdemoserver.websocket.handler.defaults.HandlerTestMessage;
+import at.aau.serg.websocketdemoserver.websocket.handler.roomTopic.HandlerJoinRoom;
 import at.aau.serg.websocketdemoserver.websocket.handler.roomTopic.HandlerOpenRoom;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -120,7 +121,7 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
                 //messages for opening, joining and listing rooms
                 //case OPEN_ROOM -> handleOpenRoomMessage(session, payload);
                 case OPEN_ROOM -> HandlerOpenRoom.handleOpenRoomMessage(session, payload);
-                case JOIN_ROOM -> handleAskForJoinRoom(session, payload);
+                case JOIN_ROOM -> HandlerJoinRoom.handleAskForJoinRoom(session, payload, roomRepo);
                 case LIST_ROOMS -> handleAskForRoomList(session, payload);
                 //messages for gameboard logic
                 case GAMEBOARD -> handleGameBoardMessage(session, payload);
@@ -132,21 +133,12 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
         }
     }
 
-    /*
-    private void handleHeartbeat(WebSocketSession session, String payload) throws Exception {
-        HeartbeatMessage heartbeatMessage = gson.fromJson(payload, HeartbeatMessage.class);
+    public void handleGameBoardMessage(WebSocketSession session, String payload) throws Exception {
+        //TODO: Task Flo...
+        //TODO: at the moment empty class, that no error occur
+    }
 
-        if (heartbeatMessage != null && heartbeatMessage.getText().equals("ping")) {
-            heartbeatMessage.setText("pong");
-            String response = gson.toJson(heartbeatMessage);
-            session.sendMessage(new TextMessage(response));
-            System.out.println("valid message");
 
-        }
-        else {
-            System.out.println("invalid message");
-        }
-    }*/
 
     public void handleChatMessage(WebSocketSession session, String payload) throws Exception {
                 //TODO:
@@ -198,25 +190,7 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
                 broadcastMsg(payloadExport, session);
             }
 
-    //TODO: delete
-           /*
-    private void handleSetupField(WebSocketSession session, String payload) throws Exception {
-        Gson gson = new Gson();
-        RoomMessage roomMessage = gson.fromJson(payload, RoomMessage.class);
-        Room room = roomRepo.findRoomById(roomMessage.getRoomID());
 
-        ArrayList<Spieler> playerList = room.getListOfPlayers();
-        Random random = new SecureRandom();
-        int playerToStart = random.nextInt(4) + 1;
-        roomMessage.setCurrentPlayer(playerList.get(playerToStart));
-        roomMessage.setPlayerIndex(playerToStart);
-
-        Gameboard gameboard = new Gameboard(playerList.size());
-        roomMessage.setGameboard(gameboard);
-
-        String positivePayload = gson.toJson(roomMessage);
-        session.sendMessage(new TextMessage(positivePayload));
-    }*/
 
     private void handleGuessCheater(WebSocketSession session, String payload) throws Exception {
         Gson gson = new Gson();
@@ -224,109 +198,10 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
     }
 
 
-    //TODO: delete
-    private void handleNextPlayer(WebSocketSession session, String payload) throws Exception {
-        Gson gson = new Gson();
-        RoomMessage roomMessage = gson.fromJson(payload, RoomMessage.class);
-        ArrayList<Player> PlayerList;
-        PlayerList = roomMessage.getListOfPlayers();
-        int playerIndex = roomMessage.getPlayerIndex();
-        roomMessage.setNextPlayer(PlayerList.get((playerIndex + 1) % PlayerList.size()));
-        roomMessage.setCurrentPlayer(roomMessage.getNextPlayer());
-
-        String positivePayload = gson.toJson(roomMessage);
-        session.sendMessage(new TextMessage(positivePayload));
-    }
-
-    //TODO: delete
-    private void handleGameBoardMessage(WebSocketSession session, String payload) throws Exception {
-        Gson gson = new Gson();
-        RoomMessage roomMessage = gson.fromJson(payload, RoomMessage.class);
-
-        switch (roomMessage.getActionType()) {
-            case DRAWCARD -> handleDrawCard(session, payload);
-            case CHAT -> handleChatMessage(session, payload);
-            case GUESSCHEATER -> handleGuessCheater(session, payload);
-            case NEXTPlAYER -> handleNextPlayer(session, payload);
-        }
-    }
-
-    /*private void handleOpenRoomMessage(WebSocketSession session, String payload) throws Exception {
-        //1 extract
-        OpenRoomMessage openRoomMessage = gson.fromJson(payload, OpenRoomMessage.class);
-
-        //2 set local variables
-        String roomName = openRoomMessage.getRoomName();
-        String playerName = openRoomMessage.getPlayerName();
-        int maxPlayers = Integer.parseInt(openRoomMessage.getNumPlayers());
-
-        //3_1 checks1
-        if (roomName == null || roomName.isEmpty() || playerName == null || playerName.isEmpty() || maxPlayers < 2 || maxPlayers > 4) {
-            //fehlerhafte Werte
-            openRoomMessage.setOpenRoomActionType(OpenRoomMessage.OpenRoomActionType.OPEN_ROOM_ERR);
-            String errorPayload = gson.toJson(openRoomMessage);
-            session.sendMessage(new TextMessage(errorPayload));
-            return;
-        }
-
-        //3_2 check2
-        Room foundRoom = roomRepo.findRoomByName(roomName);
-        System.out.println("****");
-        System.out.println(foundRoom);
-        System.out.println("****");
-
-        //4 checks --> wenn der raum schon existiert...
-        if (foundRoom != null) {
-            // Room already exists
-            // 4_1 senden...
-            if (foundRoom.getRoomName().equals(openRoomMessage.getRoomName())) {
-                openRoomMessage.setOpenRoomActionType(OpenRoomMessage.OpenRoomActionType.OPEN_ROOM_ERR);
-                String errorPayload = gson.toJson(openRoomMessage);
-                session.sendMessage(new TextMessage(errorPayload));
-                System.out.println("toClient: " + errorPayload);
-                return;
-
-            }
-
-        }
-
-        //5 positive case --> der raum wird hinzugefügt
-        //else
-        //room does not exist already
-        Room roomToAdd = new Room(maxPlayers, roomName);
-        //Spieler erschaffen
-        Player creator = new Player(playerName);
-        String creatorId = creator.getPlayerID();
-        roomToAdd.addPlayer(creator);
-        roomToAdd.setCreatorName(playerName);
-        System.out.println("after adding player " + roomToAdd.getAvailablePlayersSpace());
-
-        //5_1 raum auch zur repo --> also zur DB hinzufügen
-        //add room to repo
-        roomRepo.addRoom(roomToAdd);
-
-        //5_2 message vorbereiten und senden --> serialisieren
-        //now prepare message
-        openRoomMessage.setOpenRoomActionType(OpenRoomMessage.OpenRoomActionType.OPEN_ROOM_OK);
-        openRoomMessage.setRoomId(roomToAdd.getRoomID());
-        openRoomMessage.setRoomName(roomToAdd.getRoomName());
-        openRoomMessage.setPlayerId(creatorId);
-        openRoomMessage.setPlayerName(creator.getName());
-        openRoomMessage.setNumPlayers(Integer.toString(maxPlayers));
-
-    //openRoomMessage.setMessageIdentifier(messageIdentifier);
 
 
 
-        //5_3 hier wird schlussendlich die positive nachricht versendet...
-        //prepare positive payload
-        String positivePayload = gson.toJson(openRoomMessage);
-        session.sendMessage(new TextMessage(positivePayload));
-        System.out.println("end of player hinzugefügt");
-        System.out.println("toClient: " + positivePayload);
 
-
-    }*/
 
     private void handleDrawCard(WebSocketSession session, String payload) throws Exception {
         Gson gson = new Gson();
@@ -393,69 +268,7 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
     }
 
 
-    private void handleAskForJoinRoom(WebSocketSession session, String payload) throws Exception {
-        //TODO:implement the logic here
-        /**
-         * es ist nicht notwendig, dass der name gecheckt wird
-         * es dürfen auch mehrere spieler mit gleichen namen dabei sein
-         * es muss jedoch der raum stimmen und es muss
-         * noch platz im raum sein --> hinweis availablePlayerSpace
-         * vll. braucht man einen mutex für die operation, wo ein Spieler zu einem Raum beitritt
-         * ... es könnten ja gleich mehrere potenzielle Spieler den Gedanken haben beim gleichen
-         * Raum beitreten bzw. einsteigen zu wollen...
-         * */
 
-        //1 extract
-        JoinRoomMessage joinRoomMessage = gson.fromJson(payload, JoinRoomMessage.class);
-
-        //2 set local variables
-        String roomName = joinRoomMessage.getRoomName();
-        String playerName = joinRoomMessage.getPlayerName();
-
-        //3 check
-        if (roomName == null || roomName.isEmpty() || playerName == null || playerName.isEmpty()) {
-            //fehlerhafte Werte
-            joinRoomMessage.setActionTypeJoinRoom(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_ERR);
-            String errorPayload = gson.toJson(joinRoomMessage);
-            session.sendMessage(new TextMessage(errorPayload));
-            return;
-        }
-
-        // this need to be syncronized
-
-        Room foundRoom = roomRepo.findRoomByName(roomName);
-        System.out.println("****");
-        System.out.println(foundRoom);
-        System.out.println("****");
-
-
-        // if room exists, add player to room
-        if (foundRoom == null) {
-            // raum existiert nicht
-            joinRoomMessage.setActionTypeJoinRoom(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_ERR);
-            String errorPayload = gson.toJson(joinRoomMessage);
-            session.sendMessage(new TextMessage(errorPayload));
-            System.out.println("toClient: " + errorPayload);
-            return;
-
-        }
-
-        //valid, now add player
-        Player playerToAdd = new Player(playerName);
-        foundRoom.addPlayer(playerToAdd);
-
-        //msg set
-        joinRoomMessage.setActionTypeJoinRoom(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_OK);
-        joinRoomMessage.setRoomId(foundRoom.getRoomID());
-        joinRoomMessage.setPlayerId(playerToAdd.getPlayerID());
-        joinRoomMessage.setPlayerName(playerToAdd.getName());
-        //msg send
-        String positivePayload = gson.toJson(joinRoomMessage);
-        session.sendMessage(new TextMessage(positivePayload));
-        System.out.println("toClient: " + positivePayload);
-
-
-    }
 
 
     private void handleAskForRoomList(WebSocketSession session, String payload) throws Exception {
@@ -503,27 +316,7 @@ public class WebSocketHandlerImpl implements WebSocketHandler {
 
     }
 
-    private void handleTestMessage(WebSocketSession session, String payload) throws Exception {
-        //1 user message aus der payload extrahieren
-        Gson gson = new Gson();
-        //TestMessage testMessage = gson.fromJson(payload, TestMessage.class);
-        TestMessage testMessage = gson.fromJson(payload, TestMessage.class);
 
-        System.out.println("testmessage received " + testMessage.getText());
-        //2 add additional or set information to message with set
-        testMessage.setMessageType(MessageType.TEST);
-        testMessage.setText("juhu");
-        String msgIdentifier = testMessage.getMessageIdentifier();
-        testMessage.setMessageIdentifier(msgIdentifier);
-        //make a new payload
-        String payloadExport = gson.toJson(testMessage);
-        //print to terminal
-        System.out.println("testmessage changed and send " + testMessage.getText());
-        // Send echo back to the client --> export
-        //umbedingt payload verwenden!!!!!!
-        //session.sendMessage(new TextMessage("echo from handler: " + payload));
-        session.sendMessage(new TextMessage(payloadExport));
-    }
 
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
---
+++ src/test/java/at/aau/serg/websocketdemoserver/websocket/roomTopic/HandlerJoinRoomTest.java
@@ -0,0 +1,183 @@
+package at.aau.serg.websocketdemoserver.websocket.roomTopic;
+
+import at.aau.serg.websocketdemoserver.logic.TransportUtils;
+import at.aau.serg.websocketdemoserver.model.game.Player;
+import at.aau.serg.websocketdemoserver.model.raum.Room;
+import at.aau.serg.websocketdemoserver.msg.JoinRoomMessage;
+import at.aau.serg.websocketdemoserver.repository.InMemoryRoomRepo;
+import at.aau.serg.websocketdemoserver.websocket.handler.roomTopic.HandlerJoinRoom;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class HandlerJoinRoomTest {
+
+    private static final Gson gson = new Gson ();
+    private WebSocketSession session;
+
+    private JoinRoomMessage joinRoomMessage;
+
+    private final InMemoryRoomRepo inMemoryRoomRepoTest = new InMemoryRoomRepo();
+
+    private void initTestRooms() {
+        String playerName = "FranzSissi";
+        String player2 = "Daniel";
+        Player player1 = new Player(playerName);
+        Player spieler2 = new Player(player2);
+        Room testRoom1 = new Room(2, "TestRoom");
+        testRoom1.setCreatorName(playerName);
+        testRoom1.addPlayer(player1);
+        Room testRoom2 = new Room(3, "TestRo2");
+        testRoom2.setCreatorName(playerName);
+        testRoom2.addPlayer(player1);
+        Room testRoom3 = new Room(2, "TestRo3");
+        testRoom3.setCreatorName(playerName);
+        testRoom3.addPlayer(player1);
+        testRoom3.addPlayer(spieler2);
+        //testRoom3 dient dem Test, ob ein voller Raum übermittelt wird oder nicht...
+
+
+        //add
+        inMemoryRoomRepoTest.addRoom(testRoom1);
+        inMemoryRoomRepoTest.addRoom(testRoom2);
+        inMemoryRoomRepoTest.addRoom(testRoom3);
+    }
+
+    @BeforeEach
+    public void setup() {
+        session = mock(WebSocketSession.class);
+        joinRoomMessage = new JoinRoomMessage();
+        //initialize the default rooms
+        initTestRooms();
+    }
+
+    @Test
+    public void testValidJoinRoom() throws Exception {
+
+        JoinRoomMessage joinRoomMessage = new JoinRoomMessage();
+        joinRoomMessage.setRoomName("TestRoom");
+        joinRoomMessage.setPlayerName("Julie");
+        String payload = TransportUtils.helpToJson(joinRoomMessage);
+
+        ArgumentCaptor<TextMessage> captor = ArgumentCaptor.forClass(TextMessage.class);
+
+        HandlerJoinRoom.handleAskForJoinRoom(session, payload, inMemoryRoomRepoTest);
+
+        verify(session, times(1)).sendMessage(captor.capture());
+        TextMessage responseMessage = captor.getValue();
+        JoinRoomMessage responseJoinRoomMessage = TransportUtils.helpFromJson(responseMessage.getPayload(), JoinRoomMessage.class);
+
+        assertNotNull(responseJoinRoomMessage);
+        assertEquals(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_OK, responseJoinRoomMessage.getActionTypeJoinRoom());
+        assertNotNull(responseJoinRoomMessage.getRoomId());
+        assertEquals("TestRoom", responseJoinRoomMessage.getRoomName());
+        assertEquals("Julie", responseJoinRoomMessage.getPlayerName());
+        assertNotNull(responseJoinRoomMessage.getPlayerId());
+
+    }
+
+    @Test
+    public void testInvalidJoinRoom_RoomNameDoesntExist() throws Exception {
+
+        JoinRoomMessage joinRoomMessage = new JoinRoomMessage();
+        joinRoomMessage.setRoomName("TestRoo");
+        joinRoomMessage.setPlayerName("Julie");
+        String payload = TransportUtils.helpToJson(joinRoomMessage);
+
+        ArgumentCaptor<TextMessage> captor = ArgumentCaptor.forClass(TextMessage.class);
+
+        HandlerJoinRoom.handleAskForJoinRoom(session, payload, inMemoryRoomRepoTest);
+
+        verify(session, times(1)).sendMessage(captor.capture());
+        TextMessage responseMessage = captor.getValue();
+        JoinRoomMessage responseJoinRoomMessage = TransportUtils.helpFromJson(responseMessage.getPayload(), JoinRoomMessage.class);
+
+        assertNotNull(responseJoinRoomMessage);
+        assertEquals(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_ERR, responseJoinRoomMessage.getActionTypeJoinRoom());
+        assertEquals("TestRoo", responseJoinRoomMessage.getRoomName());
+        assertEquals("Julie", responseJoinRoomMessage.getPlayerName());
+
+    }
+
+    @Test
+    public void testInvalidJoinRoom_RoomNameNULL() throws Exception {
+
+        JoinRoomMessage joinRoomMessage = new JoinRoomMessage();
+        joinRoomMessage.setRoomName(null);
+        joinRoomMessage.setPlayerName("Julie");
+        String payload = TransportUtils.helpToJson(joinRoomMessage);
+
+        ArgumentCaptor<TextMessage> captor = ArgumentCaptor.forClass(TextMessage.class);
+
+        HandlerJoinRoom.handleAskForJoinRoom(session, payload, inMemoryRoomRepoTest);
+
+        verify(session, times(1)).sendMessage(captor.capture());
+        TextMessage responseMessage = captor.getValue();
+        JoinRoomMessage responseJoinRoomMessage = TransportUtils.helpFromJson(responseMessage.getPayload(), JoinRoomMessage.class);
+
+        assertNotNull(responseJoinRoomMessage);
+        assertEquals(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_ERR, responseJoinRoomMessage.getActionTypeJoinRoom());
+        assertNull(responseJoinRoomMessage.getRoomName());
+        assertEquals("Julie", responseJoinRoomMessage.getPlayerName());
+
+    }
+
+    @Test
+    public void testInvalidJoinRoom_PlayerNameNULL() throws Exception {
+
+        JoinRoomMessage joinRoomMessage = new JoinRoomMessage();
+        joinRoomMessage.setRoomName("TestRoom");
+        joinRoomMessage.setPlayerName(null);
+        String payload = TransportUtils.helpToJson(joinRoomMessage);
+
+        ArgumentCaptor<TextMessage> captor = ArgumentCaptor.forClass(TextMessage.class);
+
+        HandlerJoinRoom.handleAskForJoinRoom(session, payload, inMemoryRoomRepoTest);
+
+        verify(session, times(1)).sendMessage(captor.capture());
+        TextMessage responseMessage = captor.getValue();
+        JoinRoomMessage responseJoinRoomMessage = TransportUtils.helpFromJson(responseMessage.getPayload(), JoinRoomMessage.class);
+
+        assertNotNull(responseJoinRoomMessage);
+        assertEquals(JoinRoomMessage.ActionTypeJoinRoom.JOIN_ROOM_ERR, responseJoinRoomMessage.getActionTypeJoinRoom());
+        assertNull(responseJoinRoomMessage.getPlayerName());
+        assertEquals("TestRoom", responseJoinRoomMessage.getRoomName());
+
+    }
+
+    @Test
+    public void sessionIsNULL () {
+        String payload = gson.toJson(joinRoomMessage);
+        assertThrows(NullPointerException.class, () -> HandlerJoinRoom.handleAskForJoinRoom(null, payload, inMemoryRoomRepoTest));
+    }
+
+    @Test
+    public void payloadIsNULL () {
+        assertThrows(NullPointerException.class, () -> HandlerJoinRoom.handleAskForJoinRoom(session, null, inMemoryRoomRepoTest));
+    }
+
+    @Test
+    public void testJsonErrMsg () {
+        String payload = "{error}";
+        assertThrows(JsonParseException.class, () -> HandlerJoinRoom.handleAskForJoinRoom(session, payload, inMemoryRoomRepoTest));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        session = null;
+        joinRoomMessage = null;
+
+    }
+
+
+
+}
